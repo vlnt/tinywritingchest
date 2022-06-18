65fbd52fd3311a3412f340e8f33cb280b2717a0a
Valentyn Mirosh <vlnt@ua.fm>
2022-06-19 01:17:05 +0300

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Footer(props) {
+    return (
+        <footer className='footer'>
+            <small className="footer-content">© 2022 vlnt development. All rights reserved.</small>
+        </footer>
+    )
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(){
         <header className="header">
             <div className="container">
             <img src="vlnt.jpg" alt="vlnt"/>
-            <h2 className="header--title">Tiny Writing Chest</h2>
+            <h2 className="header--title">TinyWritingChest</h2>
             
             </div>
         </header>
