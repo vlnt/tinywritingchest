b61ec15cf0f3cbb2d8c3a3132ad4881de8397ad9
Valentyn Mirosh <vlnt@ua.fm>
2022-06-18 23:18:41 +0300

    a bit more refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Header from "./components/header"
-import Content from './components/content'
+import Header from "./components/Header"
+import Content from './components/Content'
 
 export default function App(){
     return(
