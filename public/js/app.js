88a55649fd2f3bed38ba5754009ed3253ef56522
Valentyn Mirosh <vlnt@ua.fm>
2022-07-07 01:24:10 +0300

    attach sw

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,11 @@
-// if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker
-//       .register('/sw.js',{scope: '/'})
-//       .then(function() {
-//         console.log('Service worker registered!')
-//       }).catch(function(e){
-//         console.log(e.message)
-//       })
-//   }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js',{scope: '/'})
+      .then(function() {
+        console.log('Service worker registered!')
+      }).catch(function(e){
+        console.log(e.message)
+      })
+  }
 
   
