0d6cfcb179b8615ae9eb1dc4641b2630450be3ca
Valentyn Mirosh <vlnt@ua.fm>
2022-09-23 20:23:06 +0300

    post added

--- src/data.js
+++ src/data.js
@@ -37,4 +37,68 @@ export default [
            `,
         date: 'Thursday, June 4, 2015'
     },
+    {
+        id: 3,
+        title: "Iran:challenge of modernity.",
+        text: `By the middle of 19th century european countries had achieved considerable success in industrial production and scientific
+        innovation .From this point they started to move to democracy later in 20th century. It was different with Islamic world.
+        They had delapidated economy, badly developed army and governmental institution.First step to keep pace with the West,which, 
+        by the time ,became more and more prevailing on the internetionai stage in economy ,science and military affairs,was made by 
+        Ottoman Empire. In 1839 sultan AbdulMejid issued the Tanzimat Fermany,which means reorganization.
+        But for almost all muslim countries it was very difficult challenge,primarily psychologically,to accept the modernization,
+        as it was being conducted by 'ethnically strange outsiders'(as in colonial countries like Tunisia and Morocco) and assumed 
+        great transformations in society, which was strongly opposed due to religious principles.
+          There are four conventional ways of adaptation They have been adopted and widely accepted by scholars when dealing with
+        challenges which modernity poses before  Islamic countries.They are:
+         1. Emulation/Secularism
+         2. Religious reform
+         3. Traditionalism
+         4. Fundamentalism
+        Let us look at Iran model of adaptation .It is now believed  the country adheres the fundamentalist way.Iran have taken this way 
+        after The Islamic Revolution of 1979  when shah  Mohhamed Reza Pakhlavi,who ruled the country over last 40 years,was deposed and 
+        Shia clergy seized power.
+        Ayatholla  Ruholla Homeieni became The Supreme Leader of Islamic Republic of Iran.New constitution have been adopted where 
+        many democratic notions have been neglected.
+        It is not posible, within the confines of this tiny essay, to give the detailed consideration  about reasons which have led 
+        to the revolution but the basic historical framework and milestones on the way to it must be mentioned.From old days ulama,
+        the Shia clergy, have been exerted tremendous influence on the Persian(as country was known before WWII) society.
+        Suffice it to mention the case with tabacco concession when Grand Ayatollah Mirza Hassan Shirazi issued fatwa against smoking,
+        trading,cultivating of tabacco.From position of heavy smoker it seems moreover  impressible even stunning what influence 
+        Grand Ayatollah enjoyed in society.And it is the clergy who in cooperation with small and medium traders,as it says,-
+        'bazari people',forced to abdicate shah Mohhamad Ali whereupon the first constitution was adopted and Majles (parliament)
+        was established.
+        In the course of subsequent history many events happened which entailed irreversible changes in economy and political life.
+        Oil was discovered,ruling dinasty was changed.Former cossak brigades oficer, Reza Khan , deposed Ahmad Shah Qajar, 
+        the last Shah of the Qajar dinasty, and established a constitutional monarchy.British oil companies penetrated extensively 
+        into the country.But Iran get a miserable ratio of revenues from oil extracted in the country by British companies.
+        How the shah not tried he could not get the oil companies to pay more.
+        Secular model of development was in deep crisis and after WWII the affairs became even worse.By this time Reza Shah was deposed and his son took throne.So in 1951  the government of Mohammad Mosaddegh nationalized the oil industry.But in  course of events  they couldn't sustain the control over the country. Britain and American inteligence services made coup d'etat.The shah ,previously deposed, was reinstated and west companies get their control over oil back.It was a crusial moment in transformation of public opinion in Iran toward americans. It became now very negative  because of the role their intelligences played out in the coup d'etat.
+        Shah Mohhamad Reza tried his best to establish western way of living and pose himself as a secular ruler.He gave rights 
+        to vote to women, tried to accomplish many social programs.But his 'White Revolution' failed.He was declining to more and more 
+        coercive ruling.SAVAK(political police) was created and trained by american experts which added to people's hate to americans.
+        To this the war of 1967,where Arab nations was defeated by Israil added  disillusionment  in emulation of the Western way.
+        People objected against shah's rule.The clergy played prominent role in protest movement.Their leader Ayatollah Khomeieni 
+        was detained and then sent to exile.That was the time when he arrives at the idea of velaet-e-faqeh (The Governance of Jurist) 
+        or Islamic State.Subsequently, after revolution the constitution of Iran was written relying on his ideas.
+        1979 became crusial in iranian society.After begining of protests shah fled from country.Revolution was supported by 
+        broad political community from  all kind of leftist parties like islamic marxist and more moderate counterparts to extreme
+        fundamentalistic followers of Ayatollah Homeiene. The new power was emerging.Step by step fundamentalists take more power, 
+        forcing  the rest of the political parties to withdraw from taking decisions and eventually expelling them from politics.
+        What is a velaet-e-faqeh ? 'The fundamental difference between Islamic government and constitutional monarchies and 
+        republics is this:whereas the representatives of the people or the monarch in such regimes engage in legislation, in Islam 
+        the legislative power and competence to establish laws belongs exclusively to God Almighty. The Sacred Legislator of Islam 
+        is the sole legislative power.' as Ayatollah Khomeiene stated in his work.
+        In the first weeks after revolution Khomeiene does much legistlative work.He announces that the Family Protection Law of 1967 
+        is abrogated, bans women to be a judges and obliges to wear hejab in the workplace.Although the right to vote  for women wasn't 
+        abolished  many other democratic freedoms never became a constitutional norm.Islam is a state religion.The only religions 
+        allowed exept islam are:Christianity,Judaism and Zoroastrism the others strictly prohibited.
+        After expelling from office the first elected president,Abolhassan Bonisadr, for incompetence, as was said,
+        the power in country was entirely seized by clerics.Their power  is sustained ,by now, with a row of not elected institution 
+        like the Council of Guardians and the Expediency Council which elect a Supreme Leader.For supression of dissent have been 
+        established the Special Court of Clergy,which isn't subordinate to any branch of power except the Supreme Leader.
+        Clergy enjoys immence influence in public life.They subordinated to themselves all economic development, army and govermental 
+        institutions.As Ayatollah Khomeiene used to say:'Islamic goverment does not correspond to any of the existing forms of government'. 
+        And it's true indeed. As it is approached in media we see in Iran an emergence of new form of goverment,-the clerical oligarchy.'`,
+        date: "Tuesday, November 4, 2014",
+    }
 ]
