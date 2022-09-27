235b499b1db0dfd33ac540d2106049c2f053f507
Valentyn Mirosh <vlnt@ua.fm>
2022-09-27 19:58:35 +0300

    add and link firebase

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './utils/firebase'
 import App from './App'
 import './style.css'
 
---
+++ src/utils/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyApKywuBDi1rHMmveIlX-YWKKCSG8JeRLk",
+  authDomain: "tinywritingchest.firebaseapp.com",
+  databaseURL: "https://tinywritingchest-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "tinywritingchest",
+  storageBucket: "tinywritingchest.appspot.com",
+  messagingSenderId: "971883656591",
+  appId: "1:971883656591:web:a1b0e3797f41e6d107c7fa",
+  measurementId: "G-WE6Y3LVGLC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const analytics = getAnalytics(app);
