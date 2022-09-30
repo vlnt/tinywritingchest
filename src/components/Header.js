56495fbb858bd3bb80ab115cb29ba5b74656f352
Valentyn Mirosh <vlnt@ua.fm>
2022-09-30 22:50:46 +0300

    login link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -11,7 +11,7 @@ import theme from '../themes'
 import { ThemeProvider } from "@emotion/react";
 //import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-export default function Header(){
+export default function Header() {
 
   // function googleSignIn(){
   //   const auth = getAuth()
@@ -34,37 +34,39 @@ export default function Header(){
   //   // ...
   // });
   //}
-    return(
-        <header className="header">
-        <ThemeProvider theme={theme}>
+  return (
+    <header className="header">
+      <ThemeProvider theme={theme}>
         <Box sx={{ flexGrow: 1 }}>
           <AppBar position="static">
             <Toolbar>
-           <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          TinyWritingChest
-          </Typography>
-          <Link to='/login'><Button id= "google_auth" color="inherit">Login</Button></Link>
-           </Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                TinyWritingChest
+              </Typography>
+              
+                <Link to='/login'>Login</Link>
+              
+            </Toolbar>
           </AppBar>
-         </Box>
-        </ThemeProvider>
-        
+        </Box>
+      </ThemeProvider>
 
-            {/* <div className="container">
+
+      {/* <div className="container">
             <img src="images/inkpot.png" alt="vlnt"/>
             <h2 className="header--title">TinyWritingChest</h2> */}
-            
-            {/* </div> */}
-        </header>
-    )
+
+      {/* </div> */}
+    </header>
+  )
 }
 
