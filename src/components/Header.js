9d03d09d655ff988322cc1332e0d1d69a3e8ddf4
Valentyn Mirosh <vlnt@ua.fm>
2022-10-01 21:10:43 +0300

    Icon button

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,15 +40,17 @@ export default function Header() {
         <Box sx={{ flexGrow: 1 }}>
           <AppBar position="static">
             <Toolbar>
-              <IconButton
+               <Link to='/'>
+               <IconButton
                 size="large"
                 edge="start"
                 color="inherit"
                 aria-label="menu"
                 sx={{ mr: 2 }}
-              > <Link to='/'></Link>
-                <MenuIcon />
-              </IconButton>
+              ><MenuIcon /></IconButton>
+              </Link>
+                
+              
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                 TinyWritingChest
               </Typography>
