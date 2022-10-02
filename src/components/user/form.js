b5c6ea1baf8947d49fc7a0ed0ef17ed365938f00
Valentyn Mirosh <vlnt@ua.fm>
2022-10-02 18:56:04 +0300

    register form

--- src/components/user/form.js
+++ src/components/user/form.js
@@ -14,11 +14,35 @@ class LoginForm extends Component {
              
     }
 
+    changeHandler = (e) => {
+
+    }
+
     render(){
         return(
             <>
                 <form onSubmit={ (e) => this.handleForm(e)}>
+                       <div className='form-group'>
+                        <label>Email</label>
+                        <input type="email"
+                               className='form-control'
+                               name="email"
+                               onChange={(e) => this.changeHandler(e)} >
 
+                               </input>
+                       </div>
+                       <div className='form-group'>
+                        <label>Password</label>
+                        <input type="password"
+                               className='form-control'
+                               name="password"
+                               onChange={(e) => this.changeHandler(e)} >
+
+                               </input>
+                       </div>
+                       <button type='submit' className='btn btn-primary'>
+                        {this.state.register ? 'Register' : 'Sign in'}
+                       </button>
                 </form>
             </>
         )
