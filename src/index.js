dc840d5856bd67b83a013d9fea84ae0f0691fb78
Valentyn Mirosh <vlnt@ua.fm>
2022-10-05 21:26:14 +0300

    auth state change

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './utils/firebase'
 import App from './App'
 import './style.css'
-import { onAuthStateChanged } from 'firebase/auth'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />)
 
+const auth = getAuth()
 
-onAuthStateChanged(user => {
+onAuthStateChanged(auth, user => {
     if(user){
         console.log(user)
     } else{
